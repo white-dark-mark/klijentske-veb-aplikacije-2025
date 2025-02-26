3191438c32d436a30546baedc30faa45a3e91ac3
Petar Kresoja <pkresoja@singidunum.ac.rs>
2025-02-26 13:45:02 +0100

    Add home page cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FlightService } from '../../services/flight.service';
-import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { AxiosError } from 'axios';
 import { FlightModel } from '../../models/flight.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [JsonPipe, NgIf, NgFor],
+  imports: [NgIf, NgFor, MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -15,9 +17,16 @@ export class HomeComponent {
   public error: string | null = null
 
   constructor() {
-    FlightService.getFlights()
+    FlightService.getFlights(0, 3)
       .then(rsp => this.flights = rsp.data.content)
       .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`)
   }
 
+  public formatDate(iso: string) {
+    return new Date(iso).toLocaleString('sr-RS')
+  }
+
+  public generateDestinationImage(dest: string) {
+    return `https://img.pequla.com/destination/${dest.split(' ')[0].toLowerCase()}.jpg`
+  }
 }
