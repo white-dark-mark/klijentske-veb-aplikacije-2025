ec77a224999c0f63a5c8fc81b506a14edd6d2978
Marko Stankovic <marko.stankovic@fishingbooker.com>
2025-07-02 22:53:58 +0200

    feat(remove-airline): remove alll airline like things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { SearchComponent } from './search/search.component';
-import { AirlineComponent } from './airline/airline.component';
 import { DetailsComponent } from './details/details.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
@@ -12,10 +9,9 @@ import { MoviesComponent } from './movies/movies.component';
 import { SearchMoviesComponent } from './search-movies/search-movies.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: MoviesComponent },
     { path: 'about', component: AboutComponent },
     { path: 'search', component: SearchMoviesComponent },
-    { path: 'airlines', component: AirlineComponent },
     { path: 'details/:id/order', component: OrderComponent },
     { path: 'details/:id', component: DetailsComponent },
     { path: 'login', component: LoginComponent },
